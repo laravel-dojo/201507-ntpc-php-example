e2df8c6686fc77f8dbd5b6bed140ed8a51b5ea78
Shengyou Fan <shengyoufan@gmail.com>
2015-07-05 17:09:08 +0800

    新增：示範 PHP 宣告與輸出

---
+++ 2-declarations/index.php
@@ -0,0 +1,15 @@
+<?php
+
+	$pageTitle = 'Hello, world';
+	$pageContent = 'Long long time ago...';
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>PHP 宣告與輸出 | 2015 新北市政府教育局 樹林國小 Laravel 工作坊</title>
+</head>
+<body>
+	<h1><?php echo $pageTitle ?></h1>
+	<p><?=$pageContent?></p>
+</body>
+</html>
