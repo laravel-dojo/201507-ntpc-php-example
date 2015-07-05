7a4847ec78d26956751e9125de5367ea44f78939
Shengyou Fan <shengyoufan@gmail.com>
2015-07-05 18:01:09 +0800

    新增：示範算數操作子、賦值操作及遞增和遞減

---
+++ 9-operators/index.php
@@ -0,0 +1,57 @@
+<?php
+
+echo '<pre>';
+
+// part 1
+$x = 10;
+$y = 2;
+
+$result = $x + $y;
+echo $result."\n";
+
+$result = $x - $y;
+echo $result."\n";
+
+$result = $x * $y;
+echo $result."\n";
+
+$result = $x / $y;
+echo $result."\n";
+
+echo "\n\n";
+
+
+// part 2
+$x = 20;
+
+$x += 2;
+echo $x."\n";
+
+$x -= 6;
+echo $x."\n";
+
+$x *= 5;
+echo $x."\n";
+
+$x /= 2;
+echo $x."\n";
+
+$x .= '-files.txt';
+echo $x."\n";
+
+echo "\n\n";
+
+
+// part 3
+$x = 20;
+
+$x++;
+echo $x."\n";
+
+$x--;
+echo $x."\n";
+
+
+echo '</pre>';
+
+?>
