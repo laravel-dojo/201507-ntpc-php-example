6b322bd9ad48c0572482475e1f3ff9e0a3871ed4
Shengyou Fan <shengyoufan@gmail.com>
2015-07-05 19:47:45 +0800

    新增：介面範例

---
+++ 19-interface/Swimmable.php
@@ -0,0 +1,6 @@
+<?php
+
+interface Swimmable
+{
+	public function swim();
+}
---
+++ 19-interface/index.php
@@ -0,0 +1,8 @@
+<?php
+
+require 'Animal.php';
+require 'Swimmable.php';
+require 'Dog.php';
+
+$dog = new Dog;
+$dog->swim();
